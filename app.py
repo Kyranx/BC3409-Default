a85e7e7e0ab56ddf7e691b4001707a410256472a
Alvin Lim <47299027+Kyranx@users.noreply.github.com>
2022-02-25 14:58:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
         elif model_type == "XG Boost":
             model=joblib.load("assignment_xg")
             
-        else:
+        elif model_type == "Logistic Regression":
             model=joblib.load("assignment_logreg")
             
         income=request.form.get("income")
