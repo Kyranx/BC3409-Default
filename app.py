171a47cf91ea5c7c3e9f6f57903356332e904d38
Alvin Lim <47299027+Kyranx@users.noreply.github.com>
2022-02-25 14:36:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ import joblib
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        if option value = "logreg": 
+        if request.form.get("logreg"): 
             model=joblib.load("assignment_logreg")
-        elif option value = "tree"
+        elif request.form.get("tree"):
             model=joblib.load("assignment_tree")
-        elif option value = "forest"
+        elif request.form.get("forest"):
             model=joblib.load("assignment_forest")
-        elif option value = "xg"
+        elif request.form.get("xg"):
             model=joblib.load("assignment_xg")
-        else:
+        elif request.form.get("logreg"):
             model=joblib.load("assignment_nn")
         income=request.form.get("income")
         age=request.form.get("age")
