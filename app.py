7e83cb0ca8ea3996bb9ae67e90ffae8b4919971f
Alvin Lim <47299027+Kyranx@users.noreply.github.com>
2022-02-25 14:30:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,20 @@ import joblib
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
+        if option value = "logreg": 
+            model=joblib.load("assignment_logreg")
+        elif option value = "tree"
+            model=joblib.load("assignment_tree")
+        elif option value = "forest"
+            model=joblib.load("assignment_forest")
+        elif option value = "xg"
+            model=joblib.load("assignment_xg")
+        else:
+            model=joblib.load("assignment_nn")
         income=request.form.get("income")
         age=request.form.get("age")
         loan=request.form.get("loan")
         print(income,age,loan)
-        model=joblib.load("assignment_logreg")
         pred=model.predict([[float(income),float(age),float(loan)]])
         if pred == 1:
             s = "The borrower will default"
